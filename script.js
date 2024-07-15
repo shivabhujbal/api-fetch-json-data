35d68cb4fa493ead44e50ddd850d673e985e21c8
shivabhujbal <shivsambh2001bhujbal@gmail.com>
2024-07-15 16:26:08 +0530

    Styling added

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const fetchData = async()=>{
         document.getElementById('men-button').addEventListener('click',()=>displayFilteredProducts(data.categories,'Men'));
         document.getElementById('women-button').addEventListener('click',()=>displayFilteredProducts(data.categories,'Women'));
         document.getElementById('kids-button').addEventListener('click',()=>displayFilteredProducts(data.categories,'Kids'));
-
+        document.getElementById('search-input').addEventListener('input',() => searchProducts(data.categories));
 
     }catch(err){
         console.error("Error with fetching Api",err);
@@ -24,29 +24,25 @@ function displayProducts(categories){
     productList.innerHTML='';
     categories.forEach(
         category => {
-            // const categoryTitle = document.createElement('h2');
-            // categoryTitle.textContent = category.category_name;
-            // productList.appendChild(categoryTitle)
-
-
             category.category_products.forEach(
                 product =>{
                     const productItem = document.createElement('div');
                     productItem.innerHTML = `
                     <div class="product-details">
-                    <h3>${product.title}</h3>
+                    <h3 class="mt-4">${product.title}</h3>
                         <div class="product-images">
                             <img src="${product.image}" alt="${product.title}" width="100">
                             <img src="${product.second_image}" alt="${product.title}" width="100">
                         </div>
-                    <p>Price: ${product.price}</p>
-                    <p>Compare at price: ${product.compare_at_price}</p>
-                    <p>Vendor: ${product.vendor}</p>
-                    <p>${product.badge_text}</p>
-                    <button class="btn btn-outline-danger"><b>Buy Now<b/></button>
+                        <p class="price">Price: ${product.price}</p>
+                        <p class="compare-price">Compare at price: ${product.compare_at_price}</p>
+                        <p class="vendor">Vendor: ${product.vendor}</p>
+                        <p class="badge-text">${product.badge_text}</p>
                 </div>
-                <div>
-                    
+
+                <div class="buy">
+                    <button class="btn "><b>Buy Now<b/></button>
+
                 </div>
                 
             `;
@@ -67,6 +63,34 @@ function displayFilteredProducts(cate,categoryName){
     );
     // console.log(filterdProducts);
     displayProducts(filterdProducts)
+}
+
+function searchProducts(categories){
+
+    const query = document.getElementById('search-input').value.toLowerCase();
+    const filterdProducts =[];
+
+    categories.forEach(
+        category => {
+            const machingProducts = category.category_products.filter(
+                product =>
+                    (product.title?.toLowerCase().includes(query)) ||
+                    (product.vendor?.toLowerCase().includes(query)) ||
+                    (product.price?.toLowerCase().includes(query)) ||
+                    (product.badge_text?.toLowerCase().includes(query))
+
+
+            );
+            if (machingProducts.length) {
+                filterdProducts.push({
+                    category_name:category.category_name,
+                    category_products:machingProducts
+                });
+                
+            }
+        });
+
+        displayProducts(filterdProducts)
 }
 
 
